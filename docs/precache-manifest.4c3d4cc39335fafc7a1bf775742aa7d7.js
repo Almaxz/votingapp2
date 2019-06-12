e641f6cb3d664f27b53b4caff7fb11426ebea199
Leo Zheng <ting5zheng@gmail.com>
2019-06-12 17:48:13 -0400

    initial commit

---
+++ docs/precache-manifest.4c3d4cc39335fafc7a1bf775742aa7d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76376170fee3c3219ffc20d86607bed7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5c3ed59d9f6f8683b3b",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "b5c3ed59d9f6f8683b3b",
+    "url": "/static/js/main.194aca03.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      classes: [
+        { name: "Mage", votes: 0 },
+        { name: "Archer", votes: 0 },
+        { name: "Priest", votes: 0 },
+        { name: "Warrior", votes: 0 },
+        { name: "Paladin", votes: 0 },
+        { name: "Assassin", votes: 0 },
+        { name: "Barbarian", votes: 0 },
+      ]
+    };
+  }
+
+  vote = i => {
+    let newchar = [...this.state.classes];
+    newchar[i].votes++;
+    const swap = (array, i, j) => {
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    const bubbleSort = array => {
+      for(let i = array.length - 1; i >= 0; i--) {
+        for(let j = array.length - 2; j >= 0; j--) {
+          if(array[j + 1].votes > array[j].votes) {
+              swap(array, j + 1, j);
+          }
+        }
+      }
+      return array;
+    }
+    bubbleSort(newchar);
+    this.setState({classes: newchar});
+  }
+    
+  render () {
+    return (
+      <div>
+        <h1>Vote for Characters!</h1>
+        <div className="content">
+          {
+            this.state.classes.map( (c, i) =>
+              <div className="content1" key={i}> 
+                <h4>
+                  {c.name}
+                </h4>
+                <button onClick={this.vote.bind(this, i)}>
+                  Likes: {c.votes}
+                </button>
+              </div>
+            )
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
